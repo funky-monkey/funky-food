e766b447c53072ae454f042104e48d5cbc094418
Sidney de Koning <sidney.dekoning@gmail.com>
2019-12-06 20:11:27 +0100

    Initial Commit

---
+++ Funky Food/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Funky Food
+//
+//  Created by Sidney de Koning on 06/12/2019.
+//  Copyright © 2019 Sidney de Koning. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
